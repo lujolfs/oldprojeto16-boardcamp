aa4b6be3c3cc4580896f3e10e288c1d1cdcc115b
lujolfs <lucas.j.frank@gmail.com>
2022-12-12 21:22:14 -0300

    feat: add search category function

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { create } from "../controllers/categories.controller.js";
+import { create, findAll } from "../controllers/categories.controller.js";
 
 const categoriesRouter = Router();
 
 categoriesRouter.post("/categories", create);
+categoriesRouter.get("/categories", findAll);
 
 export default categoriesRouter;
