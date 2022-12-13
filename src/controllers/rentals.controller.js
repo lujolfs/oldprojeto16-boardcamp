fe02f88c1c1716bb0a90913ca14993c820419496
lujolfs <lucas.j.frank@gmail.com>
2022-12-13 15:52:53 -0300

    feat: add finish and delete rent functions

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,15 +1,31 @@
 import { connection } from "../database/db.js";
+import dayjs from "dayjs"
 
 export async function create (req, res) {
     const { customerId, gameId, daysRented } = req.body;
+    const rentDate = dayjs().format('YYYY-MM-DD')
+    let pricePerDay
 
     try {
+        const { rows } = await connection.query(
+            `SELECT * FROM games WHERE id=$1;`,
+            [gameId]
+        );
+        pricePerDay = rows[0].pricePerDay
+    }
+
+    catch (err) {
+        res.status(500).send(err.message);
+    }
+    
+    try {
+        const originalPrice = pricePerDay * daysRented;
         await connection.query(
             `INSERT INTO rentals 
-                (customerId, gameId, daysRented) 
+                ("customerId", "gameId", "rentDate"::text, "daysRented", "returnDate", "originalPrice", "delayFee") 
             VALUES 
-                ($1, $2, $3);`,
-            [customerId, gameId, daysRented]
+                ($1, $2, $3, $4, $5, $6, $7);`,
+            [customerId, gameId, rentDate, daysRented, null, originalPrice, null]
         );
         res.sendStatus(201);
     } catch (err) {
@@ -19,38 +35,70 @@ export async function create (req, res) {
 
 export async function findAll (req, res) {
     try {
-        const { rows } = await connection.query("SELECT * FROM customers;");
-        res.send(rows);
+        const rentalsForm = await connection.query(`
+        SELECT 
+            rentals.id,
+            rentals."customerId",
+            rentals."rentDate"::text,
+            rentals."daysRented",
+            rentals."returnDate"::text,
+            rentals."originalPrice",
+            rentals."delayFee",
+            JSON_BUILD_OBJECT(
+                'id', customers.id,
+                'name', customers.name
+            ) AS customer,
+            JSON_BUILD_OBJECT(
+                'id', games.id,
+                'name', games.name,
+                'categoryId', games."categoryId",
+                'categoryName', categories.name
+            ) AS game
+        FROM 
+            customers
+        JOIN
+            rentals
+        ON
+            customers.id = rentals."customerId"
+        JOIN
+            games
+        ON
+            rentals."gameId" = games.id
+        JOIN
+            categories
+        ON
+            games."categoryId"=categories.id  `);
+        res.send(rentalsForm.rows[0]);
     } catch (err) {
         res.status(500).send(err.message);
     }
 }
 
-export async function findById (req, res) {
+export async function finish (req, res) {
     const { id } = req.params;
-
-    try {
-        const { rows } = await connection.query(
-            `SELECT * FROM customers WHERE id=$1;`,
-            [id]
-        );
-        res.send(rows);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-}
-
-export async function update (req, res) {
-    const { name, phone, cpf, birthday } = req.body;
-    const { id } = req.params;
+    const returnDate = dayjs().format('YYYY-MM-DD')
 
     try {
         await connection.query(
-            `UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;`,
-            [name, phone, cpf, birthday, id]
+            `UPDATE rentals SET "returnDate"=$1 WHERE id=$2;`,
+            [returnDate, id]
         );
         res.sendStatus(200);
     } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function deleteRent (req, res) {
+    const {id} = req.params;
+
+    try {
+        await connection.query(
+            `DELETE FROM rentals WHERE id=$1`, [id]
+        );
+        res.sendStatus(200);
+    }
+    catch (err) {
         res.status(500).send(err.message);
     }
 }
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { create, findAll, findById, update } from "../controllers/rentals.controller.js";
+import { create, deleteRent, findAll, finish } from "../controllers/rentals.controller.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/rentals", create);
 rentalsRouter.get("/rentals", findAll);
-rentalsRouter.get("/rentals/:id", findById);
-rentalsRouter.put("/rentals/:id", update);
+rentalsRouter.post("/rentals/:id/return", finish);
+rentalsRouter.delete("/rentals/:id", deleteRent);
 
 export default rentalsRouter;
