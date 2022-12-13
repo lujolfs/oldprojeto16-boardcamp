b63e60df8e4d3a99452eaa0cf1575fb7ef9fccf3
lujolfs <lucas.j.frank@gmail.com>
2022-12-12 22:19:34 -0300

    feat: add customers functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import categoriesRouter from "./routes/categories.routes.js";
 import gamesRouter from "./routes/games.routes.js";
+import customersRouter from "./routes/customers.routes.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(categoriesRouter);
 app.use(gamesRouter);
+app.use(customersRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
---
+++ src/routes/customers.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { create, findAll, findById, update } from "../controllers/customers.controller.js";
+
+const customersRouter = Router();
+
+customersRouter.post("/customers", create);
+customersRouter.get("/customers", findAll);
+customersRouter.get("/customers/:id", findById);
+customersRouter.put("/customers/:id", update);
+
+export default customersRouter;
